522fa7e036841b429f0499a968c0c7ee8af96091
Gabriel Santos <33208964+gabs111@users.noreply.github.com>
2022-10-25 18:46:35 -0300

    commit

--- Routes.jsx
+++ Routes.jsx
@@ -1,5 +1,5 @@
 import React, { Fragment, useEffect, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './src/Pages/Home';
 import Projects from './src/Pages/Projects';
 import Resume from './src/Pages/Resume';
